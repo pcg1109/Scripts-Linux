86f0d1ff2cae206b76e7f2d4848f1884658e2e68
Pablo Crespo <pcrespog1109@gmail.com>
2019-04-17 12:15:20 +0200

    Scripts actualización sistema

---
+++ act.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+  echo Script para actualizar sistema
+
+  echo Instalación de varios paquetes
+
+  sleep 1.5s
+
+  cd /home/pcg
+
+  sudo apt-get update
+
+  sudo apt-get upgrade
+
+  sudo apt-get autoremove
+
+
